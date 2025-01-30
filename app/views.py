ed9ee560adf4e28fbf6e0ba939e5ac8520fb4339
mimiws03 <pellizatrabajo@gmail.com>
2025-01-30 03:47:59 -0300

    Se agrego para que se reflejen las CARDS

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # capa de vista/presentación
 
 from django.shortcuts import redirect, render
-from .layers.services import services
+from .layers.services.services import getAllImages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
@@ -10,7 +10,7 @@ def index_page(request):
 
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
-    images = []
+    images = getAllImages()
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
